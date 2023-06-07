dccf35ca435197192de41383ba47b6a957a436dd
YENAZIGMINA <129706758+YENAZIGMINA@users.noreply.github.com>
2023-06-08 00:51:05 +0900

    Add files via upload

---
+++ s_01/main.js
@@ -0,0 +1,16 @@
+let acc = document.getElementsByClassName('accodion');
+console.log(acc[1])
+
+for(let i=0; i<acc.length; i++){
+    acc[i].addEventListener('click',function(){
+        this.classList.toggle('active')
+
+        let panel = this.nextElementSibling;
+
+        if(panel.style.maxHeight){
+            panel.style.maxHeight=null;
+        }else{
+            panel.style.maxHeight=panel.style.scrollHeight+'px';
+        }
+    })
+}
