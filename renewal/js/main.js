14d24959b45b2baf9f6f810b1ab811bf8104d7e5
YENAZIGMINA <129706758+YENAZIGMINA@users.noreply.github.com>
2023-06-30 03:15:09 +0900

    Add files via upload

---
+++ renewal/js/main.js
@@ -0,0 +1,18 @@
+//로딩 마우스 효과
+window.addEventListener('mousemove', function(e) {
+    const cursor = document.querySelector('.cursor');
+    cursor.style.top = `${e.clientY}px`;
+    cursor.style.left = `${e.clientX}px`;
+});
+
+
+//Nav 2차메뉴
+$('.cabin li').click(function(e) {
+    e.preventDefault();
+});
+$('.cabin li').mouseover(function(){
+    $(this).find('.depth').show();
+})
+$('.cabin li').mouseout(function(){
+    $(this).find('.depth').hide();
+})
